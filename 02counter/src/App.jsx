84005e1c41befdbc88b0fee6590e0c713dd06a2f
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-03-26 22:44:51 +0530

    adding 03tailwindprops

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+      console.log(counter);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+      console.log(counter);
+    }
+  };
+
+  return (
+    <>
+      <h1>Hello, cadt!</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
